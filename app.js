e2f8c5ae6eea08c7bf6f801af40ca259c2419365
code <carlos.oliveira.dev.junior@gmail.com>
2025-02-14 23:27:55 -0300

    a estrutura da task esta sendo criada

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 "use strict"
 function createTask() {
+  // mostra no console quando a função é chamada.
   console.log("CreateTask sendo executado!");
+
+  // cria uma div que serve de container para a nova tarefa
+  const task = document.createElement("div");
+  task.setAttribute("id", `task${1}`);
+  document.getElementById("container").appendChild(task);
+
+  // cria um checkbox para a tarefa
+  const buttonOfTask = document.createElement("button");
+  buttonOfTask.innerText = "Delete Task";
+  document.getElementById(`task${1}`).appendChild(buttonOfTask);
+  // fazer com que ao clicar no botão deletar a task.
+
+
+  // adiciona a tarefa na página
+  document.getElementById("container").appendChild(task);
 }
