f5e15945e64660f51d9c956d3b7965c2ae959b09
code <carlos.oliveira.dev.junior@gmail.com>
2025-02-15 08:44:01 -0300

    ids dinamicos para cada tarefa usando uma clojure

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 "use strict"
+// cria uma closuje para contar as tarefas
+function myCounter() {
+  let counter = 0;
+  return function() {
+    counter++;
+    return counter;
+  };
+}
+
+const counter = myCounter();
+
 function createTask() {
   // mostra no console quando a função é chamada.
   console.log("CreateTask sendo executado!");
-
+  
+  const counterTasks = counter();
+  
   // cria uma div que serve de container para a nova tarefa
   const task = document.createElement("div");
-  task.setAttribute("id", `task${1}`);
+  task.setAttribute("id", `task${counterTasks}`);
   document.getElementById("container").appendChild(task);
 
   // cria um checkbox para a tarefa
   const buttonOfTask = document.createElement("button");
   buttonOfTask.innerText = "Delete Task";
-  document.getElementById(`task${1}`).appendChild(buttonOfTask);
+  document.getElementById(`task${counterTasks}`).appendChild(buttonOfTask);
   // fazer com que ao clicar no botão deletar a task.
   buttonOfTask.onclick = function() {
     console.log("botão delete clicado!");
     task.remove();
   };
-
-  // preciso tornar o id das tarefas dinamico
-  // gostaria de fazer isso usando clojures porque dai não vou precisar
-  // usar variáveis globais
 
   // adiciona a tarefa na página
   document.getElementById("container").appendChild(task);
