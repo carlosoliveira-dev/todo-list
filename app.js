49daac7ff801fcb446cb6c597d56a84bc148b62e
code <carlos.oliveira.dev.junior@gmail.com>
2025-02-15 08:17:33 -0300

    botao de deletar a task foi implementado

--- app.js
+++ app.js
@@ -13,7 +13,14 @@ function createTask() {
   buttonOfTask.innerText = "Delete Task";
   document.getElementById(`task${1}`).appendChild(buttonOfTask);
   // fazer com que ao clicar no botão deletar a task.
+  buttonOfTask.onclick = function() {
+    console.log("botão delete clicado!");
+    task.remove();
+  };
 
+  // preciso tornar o id das tarefas dinamico
+  // gostaria de fazer isso usando clojures porque dai não vou precisar
+  // usar variáveis globais
 
   // adiciona a tarefa na página
   document.getElementById("container").appendChild(task);
