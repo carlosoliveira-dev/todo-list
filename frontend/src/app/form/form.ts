a7599bcb2bd908bc2a27e005bc0687a5828b9a13
Carlos <carlos.oliveira.dev.junior@gmail.com>
2025-10-19 19:02:07 -0300

    form fazendo requisição para o backend e mostrando na view

--- frontend/src/app/form/form.ts
+++ frontend/src/app/form/form.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import { Observable } from 'rxjs';
+import { TaskService, Task } from '../task-service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, AsyncPipe],
   templateUrl: './form.html',
   styleUrl: './form.css'
 })
 export class Form {
   name = new FormControl('');
+
+  tasks$!: Observable<Task[]>;
+  
+  private taskService = inject(TaskService);
+
+  constructor() {
+    effect(() => {
+      this.tasks$ = this.taskService.getTasks();
+    });
+  }
 }
