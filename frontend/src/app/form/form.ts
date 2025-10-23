74fc668bb74fc5e7a7cf95db8f9acd6d98f623fb
Carlos <carlos.oliveira.dev.junior@gmail.com>
2025-10-23 11:52:27 -0300

    form se comunicando com a lista de tarefas

--- frontend/src/app/form/form.ts
+++ frontend/src/app/form/form.ts
@@ -1,24 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { TaskList } from '../../task-list/task-list';
 
 @Component({
   selector: 'app-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, TaskList],
   templateUrl: './form.html',
   styleUrl: './form.css'
 })
 export class Form {
   myForm = new FormControl('');
+  newTask: WritableSignal<string> = signal('')
 
   clearForm() {
     this.myForm.setValue('');
   }
 
   onEnter() {
+    
+    let myFormValue = this.myForm.value;
+        
+    if (myFormValue != null) {
+      this.newTask.set(myFormValue);
+    }
+
     this.clearForm();
   }
 
-  OnButtonClicked() {
+  OnButton() {
     this.clearForm();
   }
 
--- frontend/src/task-list/task-list.ts
+++ frontend/src/task-list/task-list.ts
@@ -1,18 +1,17 @@
 import { AsyncPipe } from '@angular/common';
-import { Component, effect, inject } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Component, effect, inject, Input, WritableSignal } from '@angular/core';
 import { Task } from '../app/task/task';
 import { TaskModel, TaskService, TaskWithIDModel } from '../app/task-service';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-task-list',
-  imports: [ReactiveFormsModule, AsyncPipe, Task],
+  imports: [AsyncPipe, Task],
   templateUrl: './task-list.html',
   styleUrl: './task-list.css'
 })
 export class TaskList {
-  name = new FormControl('');
+  @Input() newTask!: WritableSignal<string>;
 
   task$!: Observable<TaskModel>;
   tasks$!: Observable<TaskWithIDModel[]>;
@@ -22,6 +21,15 @@ export class TaskList {
   constructor() {
     effect(() => {
       this.loadTasks();
+
+      if (this.newTask().trim() !== ''){
+        const t: TaskModel = {
+          description: this.newTask(),
+          done: false
+        };
+
+        this.AddTask(t);
+      }
     });
   }
   
@@ -29,25 +37,16 @@ export class TaskList {
     this.tasks$ = this.taskService.getTasks();
   }
 
-  AddTask() {
-    
-    if (this.name.value != null && this.name.value.trim() !== '') {
-      const t: TaskModel = {
-        description: this.name.value,
-        done: false
-      }
-        
-      this.task$ = this.taskService.addTask(t);
-        
-      this.task$.subscribe({
-        next: (res) => console.log('✅ Task adicionada:', res),
-        error: (err) => console.error('❌ Erro ao adicionar:', err)
-      });
-      this.loadTasks();
-    }
+  AddTask(t: TaskModel) {
+      
+    this.task$ = this.taskService.addTask(t);
+      
+    this.task$.subscribe({
+      next: (res) => console.log('✅ Task adicionada:', res),
+      error: (err) => console.error('❌ Erro ao adicionar:', err)
+    });
+    this.loadTasks();
   }
 
-  onEnter() {
-    this.AddTask();
-  }
 }
+
