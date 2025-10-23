338138e63f427fc1208f23094c40de48d9585d5d
Carlos <carlos.oliveira.dev.junior@gmail.com>
2025-10-23 15:54:59 -0300

    task-list adicionado ao app

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,13 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import { Component, effect, inject, signal, WritableSignal } from '@angular/core';
+import {AsyncPipe} from '@angular/common'
 import { RouterOutlet } from '@angular/router';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { TaskModel, TaskService, TaskWithIDModel } from './task-service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReactiveFormsModule],
+  imports: [RouterOutlet, ReactiveFormsModule, AsyncPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -12,6 +15,41 @@ export class App {
   myForm = new FormControl('');
   newTask: WritableSignal<string> = signal('')
 
+  task$!: Observable<TaskModel>;
+  tasks$!: Observable<TaskWithIDModel[]>;
+
+  private taskService = inject(TaskService);
+
+  constructor() {
+    effect(() => {
+      this.loadTasks();
+
+      if (this.newTask().trim() !== ''){
+        const t: TaskModel = {
+          description: this.newTask(),
+          done: false
+        };
+
+        this.AddTask(t);
+      }
+    });
+  }
+  
+  loadTasks() {
+    this.tasks$ = this.taskService.getTasks();
+  }
+
+  AddTask(t: TaskModel) {
+      
+    this.task$ = this.taskService.addTask(t);
+      
+    this.task$.subscribe({
+      next: (res) => console.log('✅ Task adicionada:', res),
+      error: (err) => console.error('❌ Erro ao adicionar:', err)
+    });
+    this.loadTasks();
+  }
+  
   clearForm() {
     this.myForm.setValue('');
   }
