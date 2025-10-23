a5ad50d6e1fa9652dec7cb533505d3217675b725
Carlos <carlos.oliveira.dev.junior@gmail.com>
2025-10-23 15:43:08 -0300

    form adicionado ao app

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,14 +1,34 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Form } from './form/form';
-import { TaskList } from '../task-list/task-list';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Form, TaskList],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('todo-list-frontend');
+  myForm = new FormControl('');
+  newTask: WritableSignal<string> = signal('')
+
+  clearForm() {
+    this.myForm.setValue('');
+  }
+
+  onEnter() {
+    
+    let myFormValue = this.myForm.value;
+        
+    if (myFormValue != null) {
+      this.newTask.set(myFormValue);
+    }
+
+    this.clearForm();
+  }
+
+  OnButton() {
+    this.clearForm();
+  }
+
 }
