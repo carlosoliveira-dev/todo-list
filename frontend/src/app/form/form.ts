9eff2a40684277bbf172ae2fabb0c086c43b5e5c
Carlos <carlos.oliveira.dev.junior@gmail.com>
2025-10-23 10:51:20 -0300

    componente form implementado sem comunicação com o task-list

--- frontend/src/app/form/form.ts
+++ frontend/src/app/form/form.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.html',
   styleUrl: './form.css'
 })
 export class Form {
-  
+  myForm = new FormControl('');
+
+  clearForm() {
+    this.myForm.setValue('');
+  }
+
+  onEnter() {
+    this.clearForm();
+  }
+
+  OnButtonClicked() {
+    this.clearForm();
+  }
+
 }
   
